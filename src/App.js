63a8f9c2bc9a47f28d971e514ea72c0c8d4e92ab
unknown <5251sun@gmail.com>
2021-07-30 10:53:37 +0900

    #2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types"; 
+
+const hotMovies = [
+  {
+    id: 1,
+    title: "Black Widow",
+    image: "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UY67_CR0,0,45,67_AL_.jpg",
+    rate: 6.9
+  },
+  {
+    id: 2,
+    title: "Gunpowder Milkshake",
+    image: "https://m.media-amazon.com/images/M/MV5BYTE4YzczNDQtNmQzNS00ZTZjLThiMzEtOTUzOGQ5ODQzNzY1XkEyXkFqcGdeQXVyMTA3NTQ0Mzkw._V1_UY67_CR0,0,45,67_AL_.jpg",
+    rate: 6.0
+  },
+  {
+    id: 3,
+    title: "	The Suicide Squad",
+    image: "https://m.media-amazon.com/images/M/MV5BNGM3YzdlOWYtNjViZS00MTE2LWE1MWUtZmE2ZTcxZjcyMmU3XkEyXkFqcGdeQXVyODEyMTI1MjA@._V1_UY67_CR0,0,45,67_AL_.jpg",
+    rate: 8.3
+  },
+]
+
+function Movie({title, poster, rate}) {
+  return (
+    <div>
+      <h2>{title} is fun ({rate} / 10)</h2>
+      <img src={poster} alt={title} />
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  rate: PropTypes.number
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {hotMovies.map(movie => (
+        <Movie key={movie.id} title={movie.title} poster={movie.image} rate={movie.rate} />
+      ))}
     </div>
   );
 }
