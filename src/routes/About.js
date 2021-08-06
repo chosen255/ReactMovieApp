3fd44a8d4aa0ef156fda9019aebd6d326ae7bd3b
unknown <5251sun@gmail.com>
2021-08-06 09:14:58 +0900

    #6

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about_container">
+      <span>...</span>
+      <span>...</span>
+    </div>
+  );
+}
+
+export default About;
